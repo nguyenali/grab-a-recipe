3c77d5aca4e1ebaf02aedaa539a006218ba46193
Ali Nguyen <alin57717@gmail.com>
2021-03-20 23:29:04 -0700

    display recipe information

--- script.js
+++ script.js
@@ -45,6 +45,31 @@ function getMealRecipe(e){
     e.preventDefault();
     if(e.target.classList.contains('recipe-btn')){
         let mealItem = e.target.parentElement.parentElement;
-        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`);
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => mealRecipeModal(data.meals));
     }
+}
+
+
+//create a modal
+function mealRecipeModal(meal){
+    meal =meal[0];
+    let htmll = `
+    <h2 class="recipe-title">${meal.strMeal}</h2>
+                <p class="recipe-catergory">${meal,strCategory}</p>
+                <div class="recipe-instruct">
+                    <h3>Instructions:</h3>
+                    <p>${meal.strInstructions}</p>
+                </div>
+                <div class="recipe-meal-img">
+                    <img src="${meal.strMealThumb}" alt="">
+                </div>
+                <div class="recipe=link">
+                    <a href="${meal.strYoutube}" target="_blank">Watch Video</a>
+                </div>
+            `;
+        mealDetailsContent,innerHTML = html;
+        mealDetailsContent.parentElement.classList.add
+        ('showRecipe');
 }
