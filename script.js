f2647abc4991dcb319732853d23317ffe384873a
Ali Nguyen <alin57717@gmail.com>
2021-03-21 00:05:39 -0700

    fix erroe to display food images

--- script.js
+++ script.js
@@ -1,46 +1,49 @@
 const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
-const recipecCloseBtn = document.getElementById('recipe-close-btn');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
-//event listners
+// event listeners
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
+recipeCloseBtn.addEventListener('click', () => {
+    mealDetailsContent.parentElement.classList.remove('showRecipe');
+});
 
-//get meal list that matches with the ingredients
+
+// get meal list that matches with the ingredients
 function getMealList(){
-    let serachInputTxt = document.getElementById
-    ('search-input').value.trim();
-    console.log(serachInputTxt);
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${serachInputTxt}`)
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
     .then(response => response.json())
     .then(data => {
         let html = "";
         if(data.meals){
             data.meals.forEach(meal => {
                 html += `
-                <div class="meal-item">
-                        <div class="meal-img" data-id = "${meal.idMeal}">
-                            <img src= "${meal.strMealThumb} alt="food">
+                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
                         </div>
-                        <div class="meal-name">
+                        <div class = "meal-name">
                             <h3>${meal.strMeal}</h3>
-                            <a href="#" class="recipe-btn">Get Recipe</a>
+                            <a href = "#" class = "recipe-btn">Get Recipe</a>
                         </div>
-                    </div>`
+                    </div>
+                `;
             });
             mealList.classList.remove('notFound');
-        } else {
-            html = "Sorry, we did not find any meals!";
+        } else{
+            html = "Sorry, we didn't find any meal!";
             mealList.classList.add('notFound');
         }
 
         mealList.innerHTML = html;
-    })
+    });
 }
 
 
-//get recipe of the meal 
+// get recipe of the meal
 function getMealRecipe(e){
     e.preventDefault();
     if(e.target.classList.contains('recipe-btn')){
@@ -51,25 +54,24 @@ function getMealRecipe(e){
     }
 }
 
-
-//create a modal
+// create a modal
 function mealRecipeModal(meal){
-    meal =meal[0];
-    let htmll = `
-    <h2 class="recipe-title">${meal.strMeal}</h2>
-                <p class="recipe-catergory">${meal,strCategory}</p>
-                <div class="recipe-instruct">
-                    <h3>Instructions:</h3>
-                    <p>${meal.strInstructions}</p>
-                </div>
-                <div class="recipe-meal-img">
-                    <img src="${meal.strMealThumb}" alt="">
-                </div>
-                <div class="recipe=link">
-                    <a href="${meal.strYoutube}" target="_blank">Watch Video</a>
-                </div>
-            `;
-        mealDetailsContent,innerHTML = html;
-        mealDetailsContent.parentElement.classList.add
-        ('showRecipe');
+    console.log(meal);
+    meal = meal[0];
+    let html = `
+        <h2 class = "recipe-title">${meal.strMeal}</h2>
+        <p class = "recipe-category">${meal.strCategory}</p>
+        <div class = "recipe-instruct">
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+        <div class = "recipe-meal-img">
+            <img src = "${meal.strMealThumb}" alt = "">
+        </div>
+        <div class = "recipe-link">
+            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
+        </div>
+    `;
+    mealDetailsContent.innerHTML = html;
+    mealDetailsContent.parentElement.classList.add('showRecipe');
 }
