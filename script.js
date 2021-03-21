fc65c9305ec9137ab858ea94f631475ae518033d
Ali Nguyen <alin57717@gmail.com>
2021-03-20 23:15:01 -0700

    fetch api to look for meal details

--- script.js
+++ script.js
@@ -45,5 +45,6 @@ function getMealRecipe(e){
     e.preventDefault();
     if(e.target.classList.contains('recipe-btn')){
         let mealItem = e.target.parentElement.parentElement;
-        
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`);
+    }
 }
