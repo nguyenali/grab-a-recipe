9bc907f6c0d7ff9f4cb0c8b6496bb3ce9b32792d
Ali Nguyen <alin57717@gmail.com>
2021-03-20 23:10:45 -0700

    add meal list result functionality

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const recipecCloseBtn = document.getElementById('recipe-close-btn');
 
 //event listners
 searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click', getMealRecipe);
 
 //get meal list that matches with the ingredients
 function getMealList(){
@@ -14,6 +15,35 @@ function getMealList(){
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${serachInputTxt}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        let html = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                html += `
+                <div class="meal-item">
+                        <div class="meal-img" data-id = "${meal.idMeal}">
+                            <img src= "${meal.strMealThumb} alt="food">
+                        </div>
+                        <div class="meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href="#" class="recipe-btn">Get Recipe</a>
+                        </div>
+                    </div>`
+            });
+            mealList.classList.remove('notFound');
+        } else {
+            html = "Sorry, we did not find any meals!";
+            mealList.classList.add('notFound');
+        }
+
+        mealList.innerHTML = html;
     })
+}
+
+
+//get recipe of the meal 
+function getMealRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+        let mealItem = e.target.parentElement.parentElement;
+        
 }
